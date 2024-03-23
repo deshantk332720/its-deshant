d9880271077016042b54382a8c95eb1aee7043ee
Naveen Kumar <nk332720@gmail.com>
2024-03-23 13:08:35 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter your first number", type: "number", name: "firstnumber" },
+    { message: "enter your second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the operator to calculate the sum",
+        type: "list",
+        name: "operator",
+        choices: [
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Division"
+        ],
+    }
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please select valid operator");
+}
